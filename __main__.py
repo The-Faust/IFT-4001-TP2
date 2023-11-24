a2434d9dd27fcf739619ab52ac3875e092729dea
The-Faust <vincent.martel.11235@outlook.com>
2023-11-23 19:51:47 -0500

    started shape generation

--- __main__.py
+++ __main__.py
@@ -1,4 +1,19 @@
+import logging
+
+from src.layers.application.services.data_gen_service import DataGenService
+
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+
+
 def main():
+    def test_datagen():
+        data_gen_service = DataGenService()
+        data_gen_service.generate(n_shapes=5, max_number_of_parts=3)
+
+    test_datagen()
+
     print('Hello World!')
 
 if __name__ == "__main__":
--- src/layers/application/services/data_gen_service.py
+++ src/layers/application/services/data_gen_service.py
@@ -1,32 +1,68 @@
 import logging
 from typing import Iterable, Tuple
 
+from numpy.random import randint, choice
+
 
 class DataGenService:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
 
-    def generate_rectangles(self, max_size: int):
-        pass
+    def generate(self, n_shapes: int = 10, max_number_of_parts: int = 2):
+        valid_shapes = self.generate_valid_shapes(n_shapes)
+
+        self.logger.debug(f'valid shapes are:\n    {tuple(valid_shapes)}')
+
+        shapes = self.generate_shapes(valid_shapes, max_number_of_parts)
+
+        self.logger.debug(f'shapes are:\n    {tuple(shapes)}')
+
+        return
+
+    def generate_valid_shapes(self, n_shapes: int) -> Iterable[Tuple[int, ...]]:
+        shape_lengths = [choice([1, 2, 4]) for _ in range(n_shapes)]
+
+        i = 1
+
+        for shape_length in shape_lengths:
+            yield tuple(i + j for j in range(shape_length))
+
+            i += shape_length
+
+    def generate_shapes(self, valid_shapes: Iterable[Tuple[int, ...]], max_number_of_parts: int) -> Iterable[Tuple[int]]:
+        return (self.generate_shape(valid_shape, max_number_of_parts) for valid_shape in valid_shapes)
+
+    def generate_shape(
+        self,
+        valid_shape: Tuple[int, ...],
+        max_number_of_parts: int = 2
+    ) -> Tuple[int, ...]:
+        len_shape = len(valid_shape)
+        number_of_parts = randint(1, max_number_of_parts)
+
+        i = 1
+
+        if len_shape == 4:
+            shape = []
+
+            for i in valid_shape:
+                pass
+
+        elif len_shape == 2:
+            shape = []
+            pass
+
+        else:
+            shape = (*valid_shape, )
+
+        return shape
 
     def generate_offset(
-            self,
-            max_size: int,
-            rectangles: Iterable[Tuple[int, int]]
+        self,
+        max_size: int,
+        rectangles: Iterable[Tuple[int, int]]
     ) -> Iterable[Tuple[int, int]]:
         pass
 
-    def generate_shape(
-            self,
-            rectangles: Iterable[Tuple[int, int]],
-            offsets: Iterable[Tuple[int, int]]
-    ) -> Iterable[Tuple[int, ...]]:
-        pass
-
-    def generate_valid_shapes(
-            self,
-            rectangles: Iterable[Tuple[int, int]],
-            offsets: Iterable[Tuple[int, int]],
-            shapes: Iterable[Tuple[int, ...]]
-    ) -> Iterable[Tuple[int, ...]]:
-        pass
+    def generate_rectangles(self, max_size: int, n: int) -> Iterable[Tuple[int, int]]:
+        return ((randint(1, max_size), randint(1, max_size)) for _ in range(n))
