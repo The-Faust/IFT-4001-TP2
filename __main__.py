f8094057351503b4a7fcb915a841678f64a3b26c
Diego Cabas <dcmdm2107@gmail.com>
2023-12-02 20:52:16 -0500

    fix de datagen

--- __main__.py
+++ __main__.py
@@ -8,11 +8,11 @@ logging.getLogger().setLevel(logging.DEBUG)
 
 
 def main():
-    def test_datagen():
-        data_gen_service = DataGenService()
-        data_gen_service.generate(n_shapes=5, max_number_of_parts=3)
+    # def test_datagen():
+    #     data_gen_service = DataGenService()
+    #     data_gen_service.generate(n_shapes=5, max_number_of_parts=3)
 
-    test_datagen()
+    # test_datagen()
 
     print('Hello World!')
 
