376907e261f8abb6a27d7a31532145c4de9acd5f
Diego Cabas <dcmdm2107@gmail.com>
2023-12-03 22:49:57 -0500

    trnasfert de donnes entre les models

--- src/layers/application/services/data_gen_service.py
+++ src/layers/application/services/data_gen_service.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from pathlib import Path
 from typing import Tuple, Union, List
 
-from minizinc import Solver, Instance
+from minizinc import Solver, Instance, Result
 from numpy.random import randint
 
 from src.layers.application.factories.shape_gen_model_factory import ShapeGenModelFactory
@@ -22,7 +22,7 @@ class DataGenService:
         solver: str = 'chuffed',
         min_bounding_box_limits: Tuple[int, int] = (1, 1),
         max_bounding_box_limits: Tuple[int, int] = (5, 5),
-        timeout: int = 300,
+        timeout: int = 600,
         files: Union[str, Path] = None
     ) -> any:
         """
@@ -40,35 +40,46 @@ class DataGenService:
         solver = Solver.lookup(solver)
         model = self.shape_gen_model_factory.make_data_gen_model(files)
 
-        (bounding_box, dimension, n_shapes, surface_per_shapes, rectangles_count_per_shape) = self \
+        (bounding_box, n_shapes, surface_per_shapes, rectangles_count_per_shape) = self \
             .randomly_generate_shape_model_input(min_bounding_box_limits, max_bounding_box_limits)
 
         self.logger.debug(
             f'bounding_box={bounding_box}\n'
-            f'dimension={dimension}\n'
             f'n_shapes={n_shapes}\n'
             f'surface_per_shapes={surface_per_shapes}\n'
             f'rectangles_count_per_shape={rectangles_count_per_shape}'
         )
 
         instance = Instance(solver, model)
-        instance['boundingBox'] = bounding_box  # [4, 3]
-        instance['dimensions'] = dimension  # 2
-        instance['nShapes'] = n_shapes  # 4
-        instance['surfacePerShape'] = surface_per_shapes  # [3, 4, 1, 2]
-        instance['rectanglesCountPerShape'] = rectangles_count_per_shape  # [2, 2, 1, 1]
+        instance["boundingBox"] = bounding_box  # [4, 3]
+        instance["nShapes"] = n_shapes  # 4
+        instance["surfacePerShape"] = surface_per_shapes  # [3, 4, 1, 2]
+        instance["rectanglesCountPerShape"] = rectangles_count_per_shape  # [2, 2, 1, 1]
 
-        solution = instance.solve(timeout=timedelta(timeout), free_search=True).solution
+        solution = instance.solve(timeout=timedelta(timeout), free_search=True)
+        #self.logger.debug(solution)
 
-        self.logger.debug(solution)
+        (n_objects,n_rect,n_shapes,rect_size,rect_offset,shape,valid_shapes,l,u) = self \
+            .solution_to_packing_data(instance, solution)
 
-        return solution
+        data = {
+            "nObjects": n_objects,
+            "nRectangles": n_rect,
+            "nShapes": n_shapes,
+            "rectSize": rect_size,
+            "rectOffset": rect_offset,
+            "shape": shape,
+            "validShapes": valid_shapes,
+            "l": l,
+            "u": u
+        }
+        return data
 
     def randomly_generate_shape_model_input(
         self,
         min_bounding_box_limits: Tuple[int, int] = (1, 1),
         max_bounding_box_limits: Tuple[int, int] = (5, 5)
-    ) -> Tuple[List[int], int, int, List[int], List[int]]:
+    ) -> Tuple[List[int], int, List[int], List[int]]:
         """
 
         Args:
@@ -86,7 +97,7 @@ class DataGenService:
             randint(min_bounding_box_limits[1], max_bounding_box_limits[1])
         ])
 
-        n_shape: int = randint(1, bounding_box[0] * bounding_box[1])
+        n_shape: int = randint(1, (bounding_box[0] * bounding_box[1])+1)
 
         surface_per_shape: List[int] = list([
             (randint(1, bounding_box[0]) if 1 < bounding_box[0] else 1)
@@ -100,4 +111,27 @@ class DataGenService:
             for i in range(n_shape)
         ])
 
-        return bounding_box, 2, n_shape, surface_per_shape, rectangles_count_per_shape
+        return bounding_box, n_shape, surface_per_shape, rectangles_count_per_shape
+
+    def solution_to_packing_data(self, instance: Instance,solution: Result)\
+            ->tuple[int,int,int,List[List[int]],List[List[int]],[{int}],[{int}],[int],[int]]:
+        n_objects: int = instance["nShapes"]
+        n_rect: int = solution["rectanglesCount"]
+        rectangles_count_per_shape: List[int] = instance["rectanglesCountPerShape"]
+        n_shapes: int = n_objects
+        rect_size: List[List[int, int]] = solution["rect_size"][:n_rect]
+        rect_offset: List[List[int, int]] = solution["real_rect_offset"][:n_rect]
+        shape: [{int}] = [set(solution["shape"][i][:rectangles_count_per_shape[i]]) for i in range(n_objects)]
+        valid_shapes: [{int}] = [{i+1} for i in range(n_objects)]
+
+        return n_objects,n_rect,n_shapes,rect_size,rect_offset,shape,valid_shapes,[0,0],[4, 4]
+
+    # def rotate_shapes(self,  shape: List[List[int]], rect_size: List[List[int, int]]):
+    #     for i in range(len(shape)):
+    #         if len(shape[i]) > 1:
+    #             pass
+    #         else:
+    #             if rect_size[shape[i][0],0] > rect_size[shape[i][0],1]:
+    #                 pass
+    #             elif rect_size[shape[i][0],0] < rect_size[shape[i][0],1]:
+    #                 pass
--- src/layers/application/services/packing_service.py
+++ src/layers/application/services/packing_service.py
@@ -1,9 +1,49 @@
 import logging
+from datetime import timedelta
+from pathlib import Path
+from typing import Tuple, Union, List
 
+from minizinc import Solver, Instance, Model
+from numpy.random import randint
+
+from src.layers.application.factories.packing_model_factory import PackingModelFactory
 
 class PackingService:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
+        self.packing_model_factory: PackingModelFactory = PackingModelFactory()
 
-    def solve(self):
-        pass
+    def solve(
+            self,
+            data : dict,
+            solver: str = 'chuffed',
+            timeout: int = 600,
+            files: Union[str, Path] = None
+    )-> any:
+        solver = Solver.lookup(solver)
+        #model = self.packing_model_factory.make_packing_model(files)
+        model = Model("minizinc/packing/model.mzn")
+        instance = Instance(solver, model)
+        #
+        for key in data.keys():
+            instance[key] = data[key]
+        # rectSize: List[List[int, int]] = [[1, 1], [1, 1]]
+
+        # instance["nObjects"] = 1
+        # instance["nRectangles"] = 1
+        # instance["nShapes"] = 1
+        # instance["rectSize"] = [[1,1]]#data["rectSize"]
+        # instance["rectOffset"] = [[1,1]]#data["rectOffset"]
+        # instance["shape"] = data["shape"]
+        # instance["validShapes"] = data["validShapes"]
+        # instance["l"] = [0,0]
+        # instance["u"] = [10,10]
+
+
+        solution = instance.solve(timeout=timedelta(timeout), free_search=True)
+        print(solution)
+
+        return solution
+
+
+
